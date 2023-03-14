2dd6ef1c841413eac4fcbfd875c4b6e9c95d685f
Tariyel <islami.tariyel@gmail.com>
2023-03-15 03:46:58 +0400

    Initial commit

---
+++ app/src/main/java/com/tis/jetpackcompose_twitter/TwitterApp.kt
@@ -0,0 +1,8 @@
+package com.tis.jetpackcompose_twitter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TwitterApp : Application() {
+}
---
+++ app/src/main/java/com/tis/jetpackcompose_twitter/presentation/screens/main/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.tis.jetpackcompose_twitter.presentation.screens.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tis.jetpackcompose_twitter.presentation.screens.authentication.login.LoginScreen
+import com.tis.jetpackcompose_twitter.presentation.screens.authentication.login.theme.JetpackCompose_TwitterTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackCompose_TwitterTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
